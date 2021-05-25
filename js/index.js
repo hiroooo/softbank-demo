e84c0282d8481b83dbe9444a59d0c84c6d3f0482
鈴木浩章 <hroaki.suzuki@gmail.com>
2021-05-25 16:15:52 +0900

    Update - fontaswesome導入

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
-// $('button').click(function() {
-//   alert('test2');
-// });
-
-function scrolltop(){
-  $(window).scrollTop(10);
-}
 jQuery(function($) {
-// 画面スクロール（上へ戻る）
+
+  // スクロール
+  $('.scrollTop').click(function() {
+    $("html,body").animate({scrollTop: 0}, 2000);
+  })
+
 });
